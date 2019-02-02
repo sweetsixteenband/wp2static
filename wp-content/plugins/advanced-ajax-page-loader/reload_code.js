43db2b763f90ea3dfdc1d5506d43b007400e5be2
sweetsixteenband <47268960+sweetsixteenband@users.noreply.github.com>
2019-02-02 10:43:27 +0100

    WP2Static CREATE wp-content/plugins/advanced-ajax-page-loader/reload_code.js

---
+++ wp-content/plugins/advanced-ajax-page-loader/reload_code.js
@@ -0,0 +1,26 @@
+function AAPL_reload_code() {
+//This file is generated from the admin panel - dont edit here! 
+jQuery("a.fbPhoto").fancybox({
+	"transitionIn"	: "elastic",
+	"transitionOut"	: "elastic",
+	"titlePosition" : "inside",
+	"titleFormat"	: function(title, currentArray, currentIndex, currentOpts)
+	{
+		return "<span id=\'fancybox-title-over\' style=\'background-image:none; text-align:left;\'>" + (title.length ? title : "") + "</span>";
+	}
+});
+}
+
+function AAPL_click_code(thiss) {
+//This file is generated from the admin panel - dont edit here! 
+// highlight the current menu item
+jQuery('ul.menu li').each(function() {
+	jQuery(thiss).removeClass('current-menu-item');
+});
+jQuery(thiss).parents('li').addClass('current-menu-item');
+}
+
+function AAPL_data_code(dataa) {
+//This file is generated from the admin panel - dont edit here! 
+
+}
